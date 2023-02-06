88a47e4eb43b11c3e29a348a3c5ca131460f06cf
ValtierraXavier <xavier.valtierra@icloud.com>
2023-02-06 10:06:59 -0500

    add more fake contcats, fix number range issue

--- Contact.py
+++ Contact.py
@@ -17,5 +17,16 @@ class contacts(BaseModel):
 db.drop_tables([contacts])
 db.create_tables([contacts])
 
-xavier = contacts(first_name = 'Xavier', last_name = 'valt', number = 8625549989, type = 'mobile')
+xavier = contacts(first_name = 'Xavier', last_name = 'Valt', number = 8625549989, type = 'mobile')
 xavier.save()
+jasmine = contacts(first_name = 'Jasmine', last_name = 'Mil', number = 8627774779, type = 'mobile')
+xavier.save()
+xavier = contacts(first_name = 'Jason', last_name = 'Mil', number = 9735549989, type = 'mobile')
+xavier.save()
+xavier = contacts(first_name = 'Fred', last_name = 'Mers', number = 6467788899, type = 'Sat')
+xavier.save()
+xavier = contacts(first_name = 'Hideki', last_name = 'Nakugama', number = 9175549989, type = 'Land-Line')
+xavier.save()
+xavier = contacts(first_name = 'Jesus', last_name = 'Christ', number = 100000000, type = 'Holy')
+xavier.save()
+
